e93bb7f0bf0137c4e80537f876041b65776918eb
tybenk735 <tybenk735@gmail.com>
2026-02-20 19:12:57 +0700

    player_animation

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ import sys, pygame, Player
 pygame.init()
 size = width, height = 1280, 960
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
 black = 0, 0, 0
 
-player = pygame.image.load("Sprites/player.gif")
-playerrect = player.get_rect()
+player_run = [pygame.image.load(f"Sprites/Player/{i}.png") for i in range(1, 4)]
 
+i = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,7 +16,10 @@ while True:
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             if pygame.K_d:
                 pass
-
-    screen.fill(black)
-    screen.blit(player, )
-    pygame.display.flip()
+        screen.fill(black)
+        screen.blit(player_run[i // 6], (1000, 200))
+        i += 1
+        if i == 60:
+            i = 0
+        pygame.display.flip()
+        clock.tick(60)
